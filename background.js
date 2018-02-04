0873c2f2f41ae205a58e734e98f9d1b0a1aaf7e7
Minibrams <anders@brams.dk>
2018-02-04 16:53:17 +0100

    API key moved outside the git project, ready for individual styling of rating

--- background.js
+++ background.js
@@ -1,31 +1,32 @@
 let currentTitle = null;
 
-let API_KEY = "3b294e4";
-
-setInterval(() => {    
+setInterval(() => {
+    // Get the title card of the movie currently hovered over
     let hoveredTitle = document.getElementsByClassName("bob-title")[0];
 
+    // Only want to do this if the title card has changed - save
+    // on the avaiable API calls
     if (currentTitle != hoveredTitle &&
         hoveredTitle != undefined  ) {
 
         currentTitle = hoveredTitle;
         currentTitleJSONUrl = getAPISearchURL(currentTitle.innerHTML);
 
-        console.log("Title is: " + currentTitle.innerHTML);
-        console.log("API is: " + currentTitleJSONUrl);
-
+        // Receive the JSON object from OMDB
         $.getJSON(currentTitleJSONUrl, function (data) {
-            // Write the result to the page
+            // Write the result to the title card
             let title = currentTitle.innerHTML;
-            currentTitle.innerHTML = title + " (IMDB: " + data.imdbRating + ")";
+            let augmentedTitle = getFormattedRating(title, data.imdbRating);
+            currentTitle.innerHTML = augmentedTitle;
         });
     }
 }, 1000);
 
+// Given a movie title, returns the appropriate API url
 function getAPISearchURL(title) {
     return "https://www.omdbapi.com/?apikey=" + API_KEY + "&t=\"" + title + "\"" ; 
 }
 
-function logResults(json) {
-    console.log(json);
-}
+function getFormattedRating(title, rating) {
+    return title + "<br>(IMDB: " + rating + ")";
+} 
