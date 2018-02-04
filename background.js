976c4cb1685f00487828c69703387973fe922a65
Minibrams <anders@brams.dk>
2018-02-04 11:16:37 +0100

    Successfully fetching movie titles on hover

--- background.js
+++ background.js
@@ -1,2 +1,9 @@
 console.log("We're through boysss");
 
+setInterval(() => {    
+    let titleDivs = document.getElementsByClassName("bob-title");
+
+    for (let i = 0; i < titleDivs.length; i++) {
+        console.log(titleDivs[i].innerHTML);
+    }
+}, 10);
