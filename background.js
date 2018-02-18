78cd0111c2fe0465a8a29937f32ebe7b40ab883d
Minibrams <anders@brams.dk>
2018-02-18 18:29:30 +0100

    Added list of actors appended to title on hover

--- background.js
+++ background.js
@@ -21,7 +21,7 @@ setInterval(() => {
                          isNaN(data.imdbRating) ? "Unavailable" :
                                                   data.imdbRating;
 
-            currentTitle.innerHTML = getFormattedRating(title, data.imdbRating);
+            currentTitle.innerHTML = getFormattedRating(title, data.imdbRating, data.Actors);
         });
     }
 }, 1000);
@@ -32,10 +32,23 @@ function getAPISearchURL(title) {
     return "https://www.omdbapi.com/?apikey=" + API_KEY + "&t=\"" + title + "\"" ; 
 }
 
-function getFormattedRating(title, rating) {
+// TODO: lmfao, make this a little bit readable
+function getFormattedRating(title, rating, actors) {
     let color = rating > 8   ? "green" : 
                 rating > 6.5 ? "yellow" : 
                 rating > 5   ? "orange" : "red";
-
-    return "<span style = \"background-color: rgba(0, 0, 0, 0.4); padding: 2px 5px 2px 5px; border-radius: 5px\">IMDB: <span style = \"font-weight: bold; color: " + color + "\">" + rating + "</span></span><br>" + title;
+    let ratingDiv = "<span style = \"background-color: rgba(0, 0, 0, 0.4); padding: 2px 5px 2px 5px; border-radius: 5px\">IMDB: <span style = \"font-weight: bold; color: " + color + "\">" + rating + "</span></span><br>";
+    let actorsDiv = getFormattedActors(actors);
+    return ratingDiv + title + actorsDiv;
 } 
+
+function getFormattedActors(actors) {
+    let actorDivStart = '<span style="font-size: 12px; padding: 2px 5px 2px 5px;">(';
+    let actorDivEnd = ')</span>';
+
+    if (actors.length > 40) {
+        return actorDivStart + actors.substring(0, 35) + '...' + actorDivEnd;
+    } else {
+        return actorDivStart + actors + actorDivEnd;
+    }
+}
